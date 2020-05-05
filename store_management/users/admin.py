a9a1a13d5efbc1275469b764c7cb14ce11bc4f38
Karthik RP <blendedidea@gmail.com>
2020-05-05 09:54:49 +0530

    WIP

--- store_management/users/admin.py
+++ store_management/users/admin.py
@@ -3,13 +3,16 @@ from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 
 from store_management.users.forms import UserChangeForm, UserCreationForm
+from .models import UserRole
+
+admin.site.site_header = "Store Management"
+admin.site.register(UserRole)
 
 User = get_user_model()
 
 
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
-
     form = UserChangeForm
     add_form = UserCreationForm
     fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
